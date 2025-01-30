4e1f9aa2a274daa340a732211b5db5d4f64b74b5
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-01-30 14:18:27 +0100

    added methods to Room interface

--- src/main/java/mindescape/model/api/Room.java
+++ src/main/java/mindescape/model/api/Room.java
@@ -1,5 +1,9 @@
 package mindescape.model.api;
 
+import java.util.Set;
+
+import mindescape.api.GameObject;
+
 /**
  * Represents a room of the map.
  * 
@@ -12,4 +16,22 @@ public interface Room {
      * @return whether the player is prensent in the room or not
      */
     boolean isPlayerPresent();
+
+    /**
+     * Adds an object to the room
+     * @param gameObject
+     */
+    void addGameObject(GameObject gameObject);
+
+    /**
+     * Removes and object from the room
+     * @param gameObject
+     */
+    void removeGameObject(GameObject gameObject);
+
+    /**
+     * returns all the objects of the room
+     * @return the objects of the room as a (?, to decide)
+     */
+    Set<GameObject> getGameObjects();
 }
