7a666198af28f04c10e112b16f2243afec9112ad
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-01-31 15:15:51 +0100

    filled some methods of RoomImpl

--- src/main/java/mindescape/model/world/rooms/impl/RoomImpl.java
+++ src/main/java/mindescape/model/world/rooms/impl/RoomImpl.java
@@ -1,9 +1,12 @@
 package mindescape.model.world.rooms.impl;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import mindescape.model.world.core.api.Dimensions;
 import mindescape.model.world.core.api.GameObject;
+import mindescape.model.world.core.api.Point2D;
 import mindescape.model.world.rooms.api.Room;
 
 public class RoomImpl implements Room {
@@ -12,7 +15,13 @@ public class RoomImpl implements Room {
 
     private final Set<GameObject> gameObjects;
 
+    public RoomImpl(Dimensions dimensions, Set<GameObject> gameObjects) {
+        this.dimensions = dimensions;
+        this.gameObjects = new HashSet<>();
+        gameObjects.stream().forEach(x -> addGameObject(x));
+    }
 
+    //TODO when PlayerImpl will be ready
     @Override
     public boolean isPlayerPresent() {
         return true;
@@ -20,20 +29,25 @@ public class RoomImpl implements Room {
 
     @Override
     public void addGameObject(GameObject gameObject) {
-        if (gameObject.) {
-            
+        if (gameObject.getPosition().isPresent() && isPositionValid(gameObject.getPosition().get(), gameObject.getDimensions())) {
+            gameObjects.add(gameObject);
         }
     }
 
     @Override
     public void removeGameObject(GameObject gameObject) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeGameObject'");
+        gameObjects.remove(gameObject);
     }
 
     @Override
     public Set<GameObject> getGameObjects() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getGameObjects'");
+        return Collections.unmodifiableSet(this.gameObjects);
+    }
+
+    private boolean isPositionValid(Point2D pos, Dimensions dim) {
+        return pos.x() >= 0 &&
+            pos.y() >= 0 && 
+            pos.x() + dim.height() <= this.dimensions.height() &&
+            pos.y() + dim.height() <= this.dimensions.height();
     }
 }
