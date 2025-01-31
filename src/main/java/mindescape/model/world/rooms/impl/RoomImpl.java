42158c4647e6df3e689c19b38d23bb25dc405937
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-01-31 13:45:05 +0100

    started filling RoomImpl

---
+++ src/main/java/mindescape/model/world/rooms/impl/RoomImpl.java
@@ -0,0 +1,34 @@
+package mindescape.model.world.rooms.impl;
+
+import java.util.Set;
+
+import mindescape.model.world.core.api.GameObject;
+import mindescape.model.world.rooms.api.Room;
+
+public class RoomImpl implements Room {
+
+    @Override
+    public boolean isPlayerPresent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isPlayerPresent'");
+    }
+
+    @Override
+    public void addGameObject(GameObject gameObject) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addGameObject'");
+    }
+
+    @Override
+    public void removeGameObject(GameObject gameObject) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeGameObject'");
+    }
+
+    @Override
+    public Set<GameObject> getGameObjects() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGameObjects'");
+    }
+    
+}
