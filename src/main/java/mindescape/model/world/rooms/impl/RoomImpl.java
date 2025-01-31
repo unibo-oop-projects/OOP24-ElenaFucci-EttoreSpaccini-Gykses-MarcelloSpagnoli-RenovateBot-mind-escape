7a7d6c47637b6579e70ffc05dadf1fd570008270
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-01-31 14:49:05 +0100

    filled some methods of RoomImpl, deleted dummy

--- src/main/java/mindescape/model/world/rooms/impl/RoomImpl.java
+++ src/main/java/mindescape/model/world/rooms/impl/RoomImpl.java
@@ -2,21 +2,27 @@ package mindescape.model.world.rooms.impl;
 
 import java.util.Set;
 
+import mindescape.model.world.core.api.Dimensions;
 import mindescape.model.world.core.api.GameObject;
 import mindescape.model.world.rooms.api.Room;
 
 public class RoomImpl implements Room {
 
+    private final Dimensions dimensions;
+
+    private final Set<GameObject> gameObjects;
+
+
     @Override
     public boolean isPlayerPresent() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isPlayerPresent'");
+        return true;
     }
 
     @Override
     public void addGameObject(GameObject gameObject) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addGameObject'");
+        if (gameObject.) {
+            
+        }
     }
 
     @Override
