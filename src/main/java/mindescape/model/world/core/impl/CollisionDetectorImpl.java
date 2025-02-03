e35ae7c30886223850c364b2cf44b7a7c3c837b8
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-02-03 17:01:50 +0100

    filtered the player from the set in which collider looks for collisions

--- src/main/java/mindescape/model/world/core/impl/CollisionDetectorImpl.java
+++ src/main/java/mindescape/model/world/core/impl/CollisionDetectorImpl.java
@@ -7,12 +7,13 @@ import mindescape.model.world.core.api.CollisionDetector;
 import mindescape.model.world.core.api.Dimensions;
 import mindescape.model.world.core.api.GameObject;
 import mindescape.model.world.core.api.Point2D;
+import mindescape.model.world.player.api.Player;
 
 public class CollisionDetectorImpl implements CollisionDetector{
 
     @Override
     public Optional<GameObject> collisions(Point2D position, Dimensions dim, Set<GameObject> roomObjects) {
-        return roomObjects.stream().filter(x -> areColliding(position, dim, x)).findFirst();
+        return roomObjects.stream().filter(x -> !(x instanceof Player)).filter(x -> areColliding(position, dim, x)).findFirst();
     }
 
     private boolean areColliding(Point2D position, Dimensions dim, GameObject obj) {
