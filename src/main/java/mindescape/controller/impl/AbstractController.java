c36cc591a02aa0b54444896fe5f28cdb38b5e2c2
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2025-02-05 15:25:51 +0100

    Added Abstract controller

--- src/main/java/mindescape/controller/impl/AbstractController.java
+++ src/main/java/mindescape/controller/impl/AbstractController.java
@@ -1,42 +1,25 @@
 package mindescape.controller.impl;
 
 import mindescape.controller.api.Controller;
-import mindescape.controller.api.UserInput;
+import mindescape.controller.maincontroller.api.MainController;
 import mindescape.model.api.Model;
 import mindescape.view.api.View;
 
 public abstract class AbstractController implements Controller {
     private final View view;
     private final Model model;
-    private boolean running = true;
+    private final MainController mainController;
 
-    public AbstractController(final View view, final Model model) {
+    public AbstractController(final View view, final Model model, final MainController mainController) {
         this.view = view;
         this.model = model;
+        this.mainController = mainController;
     }
 
-    @Override
-    public void stop() {
-        this.running = false;
-    }
-
-    @Override
-    public View getView() {
-        return this.view;
-    }
-
-    @Override
-    public Model getModel() {
-        return this.model;
-    }
-
-    @Override
-    public boolean isRunning() {
-        return this.running;
-    }
-
-    public abstract void handleInput(final UserInput input);
+    public abstract void handleInput(final Object input);
 
     public abstract void loop();
+
+    public abstract void quit();
 
 }
