a159ca381df10127ce56a32b57a01886ca49d6ca
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2025-02-06 15:25:03 +0100

    gui utils

--- src/main/java/mindescape/view/world/WorldViewImpl.java
+++ src/main/java/mindescape/view/world/WorldViewImpl.java
@@ -2,6 +2,20 @@ package mindescape.view.world;
 
 import javax.swing.JPanel;
 
-public class WorldViewImpl extends JPanel {
+import mindescape.view.api.View;
+
+public class WorldViewImpl implements View {
+
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public JPanel getPanel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPanel'");
+    }
 
 }
