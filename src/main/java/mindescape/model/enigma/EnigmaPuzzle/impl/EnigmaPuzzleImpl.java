2bec071c11c04333d103834d63260105319a525c
Elena Fucci <elena.fucci3@studio.unibo.it>
2025-02-07 18:19:27 +0100

    correct errors

--- src/main/java/mindescape/model/enigma/EnigmaPuzzle/impl/EnigmaPuzzleImpl.java
+++ src/main/java/mindescape/model/enigma/EnigmaPuzzle/impl/EnigmaPuzzleImpl.java
@@ -1,5 +1,25 @@
 package mindescape.model.enigma.enigmapuzzle.impl;
 
-public class EnigmaPuzzleImpl {
+import mindescape.model.enigma.enigmapuzzle.api.EnigmaPuzzle;
+
+public class EnigmaPuzzleImpl implements EnigmaPuzzle {
+
+    @Override
+    public boolean isSolved() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isSolved'");
+    }
+
+    @Override
+    public boolean hit(Object value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hit'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
 
 }
