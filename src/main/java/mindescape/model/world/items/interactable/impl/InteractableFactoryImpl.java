b0c6599e5c05693a6bc33b198838ae3c113cec6b
Ettore Spaccini <ettore.spaccini@studio.unibo.it>
2025-02-07 15:18:55 +0100

    Added the possibility of creating a simple door always open in IntaractableFactoryImpl

--- src/main/java/mindescape/model/world/items/interactable/impl/InteractableFactoryImpl.java
+++ src/main/java/mindescape/model/world/items/interactable/impl/InteractableFactoryImpl.java
@@ -11,14 +11,8 @@ import mindescape.model.world.items.interactable.api.Unpickable;
 import mindescape.model.world.rooms.api.Room;
 
 /**
- * Implementation of the {@link InteractableFactory} interface.
- * <p>
- * Provides methods to create interactable objects in the game world, such as 
- * pickable items, doors (locked with enigmas or items), and unpickable objects 
- * with specific behaviors.
- * </p>
- *
- * @see InteractableFactory
+ * Implementation of {@link InteractableFactory} for creating interactable objects such as pickable items, doors, 
+ * and unpickable objects with specific behaviors.
  */
 public class InteractableFactoryImpl implements InteractableFactory {
 
@@ -26,11 +20,11 @@ public class InteractableFactoryImpl implements InteractableFactory {
      * Creates a pickable object with the given parameters.
      *
      * @param name        the name of the pickable object
-     * @param position    the optional position of the object in the game world
+     * @param position    the optional position in the game world
      * @param dimensions  the dimensions of the pickable object
      * @param description the description of the pickable object
      * @param id          the unique identifier of the pickable object
-     * @return a new instance of {@link Pickable}
+     * @return a new {@link Pickable} instance
      */
     @Override
     public Pickable createPickable(final String name, final Optional<Point2D> position,
@@ -39,55 +33,48 @@ public class InteractableFactoryImpl implements InteractableFactory {
     }
 
     /**
-     * Creates a door that can be unlocked using a specific pickable item.
+     * Creates a door unlocked using a specific pickable item.
      *
      * @param name            the name of the door
-     * @param position        the optional position of the door in the game world
+     * @param position        the optional position in the game world
      * @param dimensions      the dimensions of the door
      * @param key_item_id     the ID of the item required to unlock the door
-     * @param destinationRoom the room connected to the door as the destination
-     * @return a new instance of {@link Door}
+     * @param destinationRoom the destination room connected to the door
+     * @return a new {@link Door} instance
      */
     @Override
     public Door createDoorLockedWithPickable(final String name, final Optional<Point2D> position,
                                              final Dimensions dimensions, final int key_item_id,
                                              final Room destinationRoom) {
-        return new DoorLockedWithPickable(
-            new DoorImpl(position, name, dimensions, destinationRoom),
-            key_item_id
-        );
+        return new DoorLockedWithPickable(new DoorImpl(position, name, dimensions, destinationRoom), key_item_id);
     }
 
     /**
      * Creates a door that requires solving an enigma to unlock.
      *
      * @param name            the name of the door
-     * @param position        the optional position of the door in the game world
+     * @param position        the optional position in the game world
      * @param dimensions      the dimensions of the door
      * @param enigma          the enigma required to unlock the door
-     * @param destinationRoom the room connected to the door as the destination
-     * @return a new instance of {@link Door}
+     * @param destinationRoom the destination room connected to the door
+     * @return a new {@link Door} instance
      */
     @Override
     public Door createDoorLockedWithEnigma(final String name, final Optional<Point2D> position,
                                            final Dimensions dimensions, final Enigma enigma,
                                            final Room destinationRoom) {
-        return new DoorLockedWithEnigma(
-            new DoorImpl(position, name, dimensions, destinationRoom),
-            enigma
-        );
+        return new DoorLockedWithEnigma(new DoorImpl(position, name, dimensions, destinationRoom), enigma);
     }
 
     /**
-     * Creates an unpickable object that requires solving an enigma to unlock, 
-     * optionally providing a reward upon solving it.
+     * Creates an unpickable object requiring an enigma to unlock, optionally providing a reward upon solving it.
      *
      * @param name        the name of the unpickable object
-     * @param position    the optional position of the object in the game world
+     * @param position    the optional position in the game world
      * @param dimensions  the dimensions of the unpickable object
      * @param enigma      the enigma required to unlock the object
      * @param reward      an optional pickable item rewarded after solving the enigma
-     * @return a new instance of {@link Unpickable}
+     * @return a new {@link Unpickable} instance
      */
     @Override
     public Unpickable createUnpickableWithEnigma(final String name, final Optional<Point2D> position,
@@ -97,15 +84,14 @@ public class InteractableFactoryImpl implements InteractableFactory {
     }
 
     /**
-     * Creates an unpickable object that can be unlocked using a specific pickable item, 
-     * rewarding the player with another item after unlocking.
+     * Creates an unpickable object requiring a pickable item to unlock, optionally rewarding another item.
      *
      * @param name        the name of the unpickable object
-     * @param position    the optional position of the object in the game world
+     * @param position    the optional position in the game world
      * @param dimensions  the dimensions of the unpickable object
      * @param keyItem_id  the ID of the item required to unlock the object
      * @param reward      the pickable item rewarded after unlocking
-     * @return a new instance of {@link Unpickable}
+     * @return a new {@link Unpickable} instance
      */
     @Override
     public Unpickable createLockedUnpickable(final String name, final Optional<Point2D> position,
@@ -115,18 +101,32 @@ public class InteractableFactoryImpl implements InteractableFactory {
     }
 
     /**
-     * Creates an unpickable object that does not require unlocking, optionally 
-     * providing a reward when interacted with.
+     * Creates an unpickable object that does not require unlocking, optionally providing a reward.
      *
      * @param name        the name of the unpickable object
-     * @param position    the optional position of the object in the game world
+     * @param position    the optional position in the game world
      * @param dimensions  the dimensions of the unpickable object
      * @param reward      an optional pickable item rewarded upon interaction
-     * @return a new instance of {@link Unpickable}
+     * @return a new {@link Unpickable} instance
      */
     @Override
     public Unpickable createUnpickable(final String name, final Optional<Point2D> position,
                                        final Dimensions dimensions, final Optional<Pickable> reward) {
         return new UnpickableImpl(name, position, dimensions, reward);
+    }
+
+    /**
+     * Creates a simple door that is always open and does not require unlocking.
+     *
+     * @param name            the name of the door
+     * @param position        the optional position in the game world
+     * @param dimensions      the dimensions of the door
+     * @param destinationRoom the destination room connected to the door
+     * @return a new {@link Door} instance
+     */
+    @Override
+    public Door createSimpleDoor(final String name, final Optional<Point2D> position,
+                                 final Dimensions dimensions, final Room destinationRoom) {
+        return new DoorImpl(position, name, dimensions, destinationRoom);
     }
 }
