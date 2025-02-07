816b32347fbbb2bec0dcda81914e276c07c7f281
Elena Fucci <elena.fucci3@studio.unibo.it>
2025-02-07 18:21:14 +0100

    extended EnigmaPuzzleView

--- src/main/java/mindescape/view/enigmapuzzleview/impl/EnigmaPuzzleViewImpl.java
+++ src/main/java/mindescape/view/enigmapuzzleview/impl/EnigmaPuzzleViewImpl.java
@@ -1,5 +1,21 @@
 package mindescape.view.enigmapuzzleview.impl;
 
-public class EnigmaPuzzleViewImpl {
+import javax.swing.JPanel;
+
+import mindescape.view.enigmapuzzleview.api.EnigmaPuzzleView;
+
+public class EnigmaPuzzleViewImpl implements EnigmaPuzzleView{
+
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public JPanel getPanel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPanel'");
+    }
 
 }
