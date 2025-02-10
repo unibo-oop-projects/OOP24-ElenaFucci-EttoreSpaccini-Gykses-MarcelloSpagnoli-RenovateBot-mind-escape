643cc75246c5df724afdb0cdda48132f91798a40
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-02-10 12:55:24 +0100

    moved resources folder

---
+++ src/resources/tilesets/Enviroment2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.7" tiledversion="1.11.0" name="Enviroment2" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="../tiles/Tileset_16x16_2.png" width="160" height="160"/>
+</tileset>
