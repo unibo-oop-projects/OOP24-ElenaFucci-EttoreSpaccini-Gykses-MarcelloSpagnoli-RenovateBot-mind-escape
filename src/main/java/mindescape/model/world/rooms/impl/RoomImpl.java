839febe18d8823148a53f65e3cf62e74dec7c70e
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2025-02-13 11:48:58 +0100

    fixed checkstyle

--- src/main/java/mindescape/model/world/rooms/impl/RoomImpl.java
+++ src/main/java/mindescape/model/world/rooms/impl/RoomImpl.java
@@ -26,7 +26,6 @@ import mindescape.model.world.rooms.api.Room;
 public final class RoomImpl implements Room, Serializable {
 
     private final static long serialVersionUID = 1L;
-
     private final Dimensions dimensions;
     private final Set<GameObject> gameObjects = new HashSet<>();
     private final String name;
@@ -122,11 +121,11 @@ public final class RoomImpl implements Room, Serializable {
             .toList();
         rooms.forEach(room -> {
             objectsExtractor.extractfrom(room.getSource())
-                .forEach(obj -> room.addGameObject(obj));
+                .forEach(room::addGameObject);
         });
         rooms.forEach(room -> {
             objectsExtractor.addDoors(room.getSource(), rooms.stream().collect(Collectors.toSet()))
-                .forEach(door -> room.addGameObject(door));
+                .forEach(room::addGameObject);
         });
         return rooms;
     }
