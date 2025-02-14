d36284972d43277898655858653f5d6571b8c14d
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2025-02-14 12:11:38 +0100

    done

--- src/main/java/mindescape/view/utils/ViewUtils.java
+++ src/main/java/mindescape/view/utils/ViewUtils.java
@@ -266,7 +266,7 @@ public static class Style {
     public static JScrollPane createStyledScrollPane(final JLabel label) {
         final JScrollPane scrollPane = new JScrollPane(label);
         scrollPane.setBackground(Color.DARK_GRAY);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
         return scrollPane;
     }
