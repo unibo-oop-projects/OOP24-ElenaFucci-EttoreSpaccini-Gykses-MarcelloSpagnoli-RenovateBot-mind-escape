f316800c0adc5fbe57421ec65630800779025d84
Ettore Spaccini <ettore.spaccini@studio.unibo.it>
2025-02-15 18:05:05 +0100

    fixed checkstyle

--- src/test/java/mindescape/model/world/items/interactable/impl/InteractableFactoryImplTest.java
+++ src/test/java/mindescape/model/world/items/interactable/impl/InteractableFactoryImplTest.java
@@ -35,12 +35,22 @@ class InteractableFactoryImplTest {
     void setUp() {
         this.factory = new InteractableFactoryImpl();
         this.rooms = RoomImpl.createRooms();
-        this.player = new PlayerImpl(new Point2D(1, 1), "Spaccini", new Dimensions(1, 1), this.rooms.getFirst());
+        this.player = new PlayerImpl(
+                new Point2D(1, 1), 
+                "Spaccini", 
+                new Dimensions(1, 1), 
+                this.rooms.getFirst()
+            );
     }
 
     @Test
     void testCreatePickable() {
-        final Pickable pickable = factory.createPickable(KEY, new Point2D(1, 1), new Dimensions(1, 1), "A small key", 1);
+        final Pickable pickable = factory.createPickable(
+                KEY, new Point2D(1, 1), 
+                new Dimensions(1, 1), 
+                "A small key", 
+                1
+            );
         assertNotNull(pickable);
         assertEquals(KEY, pickable.getName());
         player.getInventory().addItems(pickable);
@@ -49,8 +59,20 @@ class InteractableFactoryImplTest {
 
     @Test
     void testCreateDoorLockedWithPickable() {
-        final Pickable key = factory.createPickable(KEY, new Point2D(1, 1), new Dimensions(1, 1), "A small key", 1);
-        final Door door = factory.createDoorLockedWithPickable("Locked Door", new Point2D(2, 2), new Dimensions(2, 3), 1, this.rooms.get(1), new Point2D(3, 3));
+        final Pickable key = factory.createPickable(
+                KEY, new Point2D(1, 1), 
+                new Dimensions(1, 1), 
+                "A small key", 
+                1
+            );
+        final Door door = factory.createDoorLockedWithPickable(
+                "Locked Door", 
+                new Point2D(2, 2), 
+                new Dimensions(2, 3), 
+                1, 
+                this.rooms.get(1), 
+                new Point2D(3, 3)
+            );
         assertNotNull(door);
         assertFalse(door.isUnlocked());
         player.getInventory().addItems(key);
@@ -62,7 +84,13 @@ class InteractableFactoryImplTest {
     @Test
     void testCreateDoorLockedWithEnigma() {
         final Enigma enigma = new EnigmaPasswordModelImpl("password", PASSWORD);
-        final Door door = factory.createDoorLockedWithEnigma("Enigma Door", new Point2D(2, 2), new Dimensions(2, 3), enigma, this.rooms.get(2), new Point2D(3, 3));
+        final Door door = factory.createDoorLockedWithEnigma(
+                "Enigma Door", new Point2D(2, 2), 
+                new Dimensions(2, 3), 
+                enigma, 
+                this.rooms.get(2), 
+                new Point2D(3, 3)
+            );
         assertNotNull(door);
         assertFalse(door.isUnlocked());
         enigma.hit("123");
@@ -74,8 +102,20 @@ class InteractableFactoryImplTest {
     @Test
     void testCreateUnpickableWithEnigma() {
         final Enigma enigma = new EnigmaPasswordModelImpl("password", PASSWORD);
-        final Pickable reward = factory.createPickable("Reward", new Point2D(1, 1), new Dimensions(1, 1), "A reward", 2);
-        final Unpickable unpickable = factory.createUnpickableWithEnigma("Locked Chest", new Point2D(2, 2), new Dimensions(2, 2), enigma, reward);
+        final Pickable reward = factory.createPickable(
+                "Reward", 
+                new Point2D(1, 1), 
+                new Dimensions(1, 1), 
+                "A reward", 
+                2
+            );
+        final Unpickable unpickable = factory.createUnpickableWithEnigma(
+                "Locked Chest", 
+                new Point2D(2, 2), 
+                new Dimensions(2, 2), 
+                enigma, 
+                reward
+            );
         assertNotNull(unpickable);
         assertFalse(unpickable.isUnlocked());
         enigma.hit("123");
@@ -86,9 +126,27 @@ class InteractableFactoryImplTest {
 
     @Test
     void testCreateLockedUnpickable() {
-        final Pickable key = factory.createPickable(KEY, new Point2D(1, 1), new Dimensions(1, 1), "A small key", 1);
-        final Pickable reward = factory.createPickable("Reward", new Point2D(1, 1), new Dimensions(1, 1), "A reward", 2);
-        final Unpickable unpickable = factory.createLockedUnpickable("Locked Chest", new Point2D(2, 2), new Dimensions(2, 2), 1, reward);
+        final Pickable key = factory.createPickable(
+                KEY, 
+                new Point2D(1, 1), 
+                new Dimensions(1, 1), 
+                "A small key", 
+                1
+            );
+        final Pickable reward = factory.createPickable(
+                "Reward", 
+                new Point2D(1, 1), 
+                new Dimensions(1, 1), 
+                "A reward", 
+                2
+            );
+        final Unpickable unpickable = factory.createLockedUnpickable(
+                "Locked Chest", 
+                new Point2D(2, 2), 
+                new Dimensions(2, 2), 
+                1, 
+                reward
+            );
         assertNotNull(unpickable);
         assertFalse(unpickable.isUnlocked());
         player.getInventory().addItems(key);
@@ -99,8 +157,18 @@ class InteractableFactoryImplTest {
 
     @Test
     void testCreateUnpickable() {
-        final Pickable reward = factory.createPickable("Reward", new Point2D(1, 1), new Dimensions(1, 1), "A reward", 2);
-        final Unpickable unpickable = factory.createUnpickable("Chest", new Point2D(2, 2), new Dimensions(2, 2), reward);
+        final Pickable reward = factory.createPickable(
+                "Reward", 
+                new Point2D(1, 1), 
+                new Dimensions(1, 1), 
+                "A reward", 
+                2
+            );
+        final Unpickable unpickable = factory.createUnpickable(
+                "Chest", new Point2D(2, 2), 
+                new Dimensions(2, 2), 
+                reward
+            );
         assertNotNull(unpickable);
         unpickable.onAction(player);
         assertTrue(player.getInventory().getItems().contains(reward));
@@ -108,7 +176,13 @@ class InteractableFactoryImplTest {
 
     @Test
     void testCreateSimpleDoor() {
-        final Door door = factory.createSimpleDoor("Simple Door", new Point2D(2, 2), new Dimensions(2, 3), this.rooms.get(3), new Point2D(3, 3));
+        final Door door = factory.createSimpleDoor(
+                "Simple Door", 
+                new Point2D(2, 2), 
+                new Dimensions(2, 3), 
+                this.rooms.get(3), 
+                new Point2D(3, 3)
+            );
         assertNotNull(door);
         assertTrue(door.isUnlocked());
         door.onAction(player);
